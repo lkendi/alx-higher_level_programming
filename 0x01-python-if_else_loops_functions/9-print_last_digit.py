aa260bb371e764d095e778555d9b0d531aa15133
lkendi <lkendi003@gmail.com>
2024-01-03 01:23:49 +0300

    Add newline to files

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = number % 10
     if (number < 0):
         number *= -1
         last = -1 * (number % 10)
+    else:
+        last = number % 10
     print(last, end="")
-
-
