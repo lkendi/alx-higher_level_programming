158f02ba957890ac8c9b6c52588ea477d8dbb113
lkendi <lkendi003@gmail.com>
2024-01-03 00:44:49 +0300

    Add question solutions

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,5 @@
+#!usr/bin/python3
+def remove_char_at(str, n):
+    copy = str
+    str[n] = ""
+    return copy
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if (ord(char) >= 97 and ord(char) <= 123):
+            new = ord(char) - 32
+            print(chr(new), end = "")
+        else:
+            print(char, end = "")
+    print("\n")
