cacab3a4568c2afa25f75f3c4a3bc55793b98e92
lkendi <lkendi003@gmail.com>
2024-01-03 21:58:48 +0300

    Solutions review

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+s = "is better than"
 print("""The Zen of Python, by Tim Peters\n
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
+Beautiful {0} ugly.
+Explicit {0} implicit.
+Simple {0} complex.
+Complex {0} complicated.
+Flat {0} nested.
+Sparse {0} dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
@@ -14,8 +15,9 @@ Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
+Now {0} never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!""")
+Namespaces are one honking great idea -- let's do more of those!
+""".format(s))
