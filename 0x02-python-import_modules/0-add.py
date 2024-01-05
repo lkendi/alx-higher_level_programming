88b652a86d6df99fa50741c86553fd83cba76c2d
lkendi <lkendi003@gmail.com>
2024-01-05 04:53:11 +0300

    Add solutions

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def main():
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    result = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
