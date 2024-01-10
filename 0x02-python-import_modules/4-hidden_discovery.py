9d9de32d768bd9c76a9498664b30a62d30e7bdd5
lkendi <lkendi003@gmail.com>
2024-01-10 12:48:06 +0300

    Edit 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+import marshal
 def main():
-    return 0
+    with open('hidden_4.pyc', 'rb') as file:
+        contents = file.read()
+        obj = marshal.loads(contents[16:])
+        names = obj.co_names
+        names_sorted = sorted(names)
+        for name in names_sorted:
+            if not name.startswith('__'):
+                print(name)
 
 
 if __name__ == "__main__":
