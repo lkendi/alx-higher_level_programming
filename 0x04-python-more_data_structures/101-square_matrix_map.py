ce37a1f338a73bcfc4533a30b929156f0306e6cb
lkendi <lkendi003@gmail.com>
2024-01-13 06:44:23 +0300

    Add solution 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,11 +2,11 @@
 def square_matrix_map(matrix=[]):
     """
     Computes the square of all integers of a matrix using map
-    
+
     Args:
         matrix - matrix of integers
-    
+
     Returns:
         square value of all integers of the matrix
     """
-    return list(map(lambda row: list(map(lambda num: num **2, row)), matrix))
+    return list(map(lambda row: list(map(lambda num: num ** 2, row)), matrix))
