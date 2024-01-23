74476f5846eb71e85ef235ccb4ca4be8aa6b2236
lkendi <lkendi003@gmail.com>
2024-01-23 06:16:16 +0300

    Edit solution 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -13,7 +13,7 @@ def safe_print_integer(value):
         Otherwise False
     """
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
         return True
     except ValueError:
         return False
