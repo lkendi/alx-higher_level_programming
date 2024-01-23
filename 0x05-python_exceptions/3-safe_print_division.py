d33a04253bc858500cb2e7335f2f1fbbff3e46b8
lkendi <lkendi003@gmail.com>
2024-01-23 05:38:10 +0300

    Add task 0-4 solutions

---
+++ 0x05-python_exceptions/3-safe_print_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """
+    Divides 2 integers and prints the result
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The value of division, otherwise None
+    """
+    try:
+        result = a/b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
---
+++ 0x05-python_exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Divides element by element in 2 lists
+
+    Args:
+        my_list_1: first list (can contain any type)
+        my_list_2: second list(can contain any type)
+        list_length: length of the list
+
+    Returns:
+        A new list(length = list_length) with all division
+    """
+    try:
+        result = a/b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
