5c446b7b8299c1aee747395e8b550292fa79cce0
lkendi <139500082+lkendi@users.noreply.github.com>
2024-01-25 03:20:38 +0300

    Delete 0x05-python_exceptions directory

--- 0x05-python_exceptions/6-raise_exception_msg.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def raise_exception_msg(message=""):
-    """
-    Raises a name exception with a message
-
-    Args:
-        message: message to print when the exception is raised
-    """
-    raise NameError(message)
