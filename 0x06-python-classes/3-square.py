b0abb0eb87f4f9dd1fe2adbbe0ec851234074f18
lkendi <lkendi003@gmail.com>
2024-01-27 22:02:16 +0300

    edit solutions

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Class that defines a square by:
+    Private instance attribute: size
+    Instantiation with optional size:
+        size must be an int, else TypeError
+        size must be >= 0 else ValueError
+    Has a public instance method that returns the current sqaure area
+    Without importing any module
+"""
+
+
 class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
