fffb4607706417fb03028e3ed9e5db8356f31a0a
lkendi <lkendi003@gmail.com>
2024-01-27 20:12:34 +0300

    Edit solution 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
+"""Class that defines a square by:
+    Private instance attribute: size
+    Instantiation with optional size:
+        size must be an int, else TypeError
+        size must be >= 0 else ValueError
+    Without importing any module
+"""
 class Square:
+    """Blueprint for a square object
+    
+    Attributes:
+        size(int): square size
+        
+    Methods:
+        __init__ : initializes a square object
+    """
     def __init__(self, size=0):
+        """Initializes an instance of a sqaure
+        
+        Args:
+            size - size of the square
+        
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
