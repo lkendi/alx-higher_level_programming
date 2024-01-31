47a2c50732cf75b4712abc1fdaf3fc57aae71950
lkendi <lkendi003@gmail.com>
2024-01-31 16:29:10 +0300

    Edit task 6 solution

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -20,6 +20,21 @@ class TestMaxInt(unittest.TestCase):
     def test_same_ints(self):
         """If the max integer is repeated"""
         self.assertEqual(max_integer([5, 4, 3, 2, 5, 1]), 5)
+    def test_max_at_beginning(self):
+        """If max is at the beginning of the list"""
+        self.assertEqual(max_integer([10, 9, 7, 8, 6]), 10)
+    def test_max_at_end(self):
+        """If max is at the end of the list"""
+        self.assertEqual(max_integer([6, 7, 8, 9, 10]), 10)
+    def test_max_at_center(self):
+        """If max is at the center of the list"""
+        self.assertEqual(max_integer([1, 2, 3, 2, 1]), 3)
+    def test_list_of_negatives(self):
+        """If list contains only negative values"""
+        self.assertEqual(max_integer([-1, -2, -10, -6]), -1)
+    def test_list_with_negatives(self):
+        """If list contains a mix of negative and positive values"""
+        self.assertEqual(max_integer([-1, -2, -10, -6, 1, 20, 8]), 20)
     def test_non_ints(self):
         """List that does not contain integers"""
         with self.assertRaises(TypeError):
