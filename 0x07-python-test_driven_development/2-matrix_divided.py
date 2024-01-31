d1b6db98c2a0f9ded230f5e960432a493d34ccb6
lkendi <lkendi003@gmail.com>
2024-01-31 13:44:24 +0300

    Edit task 1 solution

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,11 +21,13 @@ def matrix_divided(matrix, div):
         A new matrix with all matrix elements divided by div
         and rounded to 2 decimal places
     """
-    if not all(isinstance(row, list) and all(isinstance(element, (int, float))
-                                            for element in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) and
+               all(isinstance(element, (int, float)) for element in row)
+               for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists)of integers/floats")
 
-    if not all (len(row) == len(matrix[0]) for row in matrix):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
