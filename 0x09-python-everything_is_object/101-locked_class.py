4ecc132d9a892b552dde4aedd1feeea2de410947
lkendi <lkendi003@gmail.com>
2024-02-01 01:27:53 +0300

    Add task 30 solution

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python3
+"""
+LockedClass module
+"""
+
 
 class LockedClass:
+    """Class that restricts creation of new instance attribute
+
+    Attributes:
+        __slots__: Tuple containing the allowed instance attribute names
+                    (in this case - 'first_name')
+    Mathods:
+        __setattr__: controls attribute creation
+    """
+
     __slots__ = ('first_name',)  # Allow only 'first_name' attribute
 
     def __setattr__(self, name, value):
+        """Function that overrides the default behavior of __setattr__
+        in order to restric the creation of instance attributes
+
+        Args:
+            name(str): name of attribute to set
+            value: valueto assign to the attribute
+
+        Raises:
+            AttributeError: if attribute name is not first_name
+        """
         if name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'"
                                  .format(name))
