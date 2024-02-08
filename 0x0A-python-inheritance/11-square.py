9eb48e99beff3cb876c926c2f8ba7a6de1704568
lkendi <lkendi003@gmail.com>
2024-02-08 10:41:45 +0300

    Edit task 11 solution

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -26,6 +26,7 @@ class Square (Rectangle):
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         """Returns a string representation of the square"""
