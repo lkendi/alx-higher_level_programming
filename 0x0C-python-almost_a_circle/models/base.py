de2a0d229dfa34068b5531e272c73a9b0ce99add
lkendi <lkendi003@gmail.com>
2024-02-21 07:54:32 +0300

    Add task 20 and 21 files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 
 
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -86,3 +88,88 @@ class Base:
                 return [cls.create(**obj) for obj in dict_list]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writes the list_objs to a CSV file
+
+        Args:
+            list_objs: list of instances
+        """
+        if list_objs is None:
+            list_objs = []
+        file = cls.__name__ + ".csv"
+        with open(file, "w", newline='') as f:
+            writer = csv.writer(f, delimiter=",")
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow(
+                        [obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Loads instances list from CSV file"""
+        file = cls.__name__ + ".csv"
+        try:
+            instances = []
+            with open(file, "r", newline='') as f:
+                reader = csv.reader(f, delimiter=',')
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        my_dict = {'id': int(row[0]), 'width': int(row[1]),
+                                   'height': int(row[2]), 'x': int(row[3]),
+                                   'y': int(row[4])}
+                    elif cls.__name__ == "Square":
+                        my_dict = {'id': int(row[0]), 'size': int(row[1]),
+                                   'x': int(row[2]), 'y': int(row[3])}
+                    instance = cls.create(**my_dict)
+                    instances.append(instance)
+                return instances
+        except FileNotFoundError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Opens a window and draws all the Rectangles and Squares
+        using the Turtle graphics module
+
+        Args:
+            list_rectangles: list of rectangle instances
+            list_squares: list of square instances
+        """
+        window = turtle.Screen()
+        pen = turtle.Turtle()
+        pen.penup()
+        pen.speed(1)
+
+        for rect in list_rectangles:
+            pen.color("red")
+            pen.begin_fill()
+            pen.goto(rect.x, rect.y)
+            pen.pendown()
+            pen.forward(rect.width)
+            pen.right(90)
+            pen.forward(rect.height)
+            pen.right(90)
+            pen.forward(rect.width)
+            pen.right(90)
+            pen.forward(rect.height)
+            pen.end_fill()
+            pen.penup()
+
+        for sq in list_squares:
+            pen.color("blue")
+            pen.begin_fill()
+            pen.goto(sq.x, sq.y)
+            pen.pendown()
+            for _ in range(4):
+                pen.forward(sq.size)
+                pen.right(90)
+            pen.end_fill()
+            pen.penup()
+
+        window.exitonclick()
+
+
