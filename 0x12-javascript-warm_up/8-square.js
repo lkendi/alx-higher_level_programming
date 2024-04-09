9c62817b39ade0095a2346c52e2ec039fed079cc
lkendi <lkendi003@gmail.com>
2024-04-09 04:54:41 +0300

    Add files

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-let size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
+
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
