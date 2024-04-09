1a15029eb8552e11b056d0356a5f088062e411af
lkendi <lkendi003@gmail.com>
2024-04-09 05:50:38 +0300

    Edit file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (!isNaN(size)) {
   for (let i = 0; i < size; i++) {
     let row = '';
     for (let j = 0; j < size; j++) {
-      row += 'x';
+      row += 'X';
     }
     console.log(row);
   }
