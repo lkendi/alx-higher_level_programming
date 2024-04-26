2d85e58bdedb3d6a569a652b3dacac6ca9e5ee5a
lkendi <lkendi003@gmail.com>
2024-04-26 05:55:35 +0300

    Edit 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,24 +8,29 @@ def find_peak(list_of_integers):
     """Finds a peak in an unsorted list of integers
 
     Args:
-        list_of_integers: unsorte list of integers
+        list_of_integers: unsorted list of integers
 
     Returns:
         The peak in the list or None
     """
 
-    if len(list_of_integers) <= 1 or list_of_integers is None:
+    if len(list_of_integers) < 1 or list_of_integers is None:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
     else:
         left = 0
         right = len(list_of_integers) - 1
 
-        while (left <= right):
+        while left <= right:
             mid = (left + right) // 2
-            if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-                (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
-                     return list_of_integers[mid]
-            elif (mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]):
-                 right = mid - 1
+            if (mid == 0 or
+                list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+                    (mid == len(list_of_integers) - 1 or
+                     list_of_integers[mid] >= list_of_integers[mid + 1]):
+                return list_of_integers[mid]
+            elif (mid > 0 and
+                    list_of_integers[mid] < list_of_integers[mid - 1]):
+                right = mid - 1
             else:
-                 left = mid + 1
+                left = mid + 1
